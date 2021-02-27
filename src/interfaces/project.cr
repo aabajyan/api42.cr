06f1cf69c4ff76c7742f7eefddb6b96c6ac6d30c
aabajyan <arsen.abajyan@pm.me>
2021-02-28 01:05:03 +0400

    [FIX] Put Project into Api42 Module.
    And add info in shards.yml

--- src/interfaces/project.cr
+++ src/interfaces/project.cr
@@ -1,24 +1,26 @@
-class Project
-  include JSON::Serializable
+module Api42
+  class Project
+    include JSON::Serializable
 
-  property id : UInt64
-  property parent_id : UInt64?
-  property name : String
-  property slug : String
-end
+    property id : UInt64
+    property parent_id : UInt64?
+    property name : String
+    property slug : String
+  end
 
-class ProjectUser
-  include JSON::Serializable
+  class ProjectUser
+    include JSON::Serializable
 
-  property id : UInt64
-  property occurrence : Int32
-  property final_mark : Int32?
-  property status : String
-  property current_team_id : UInt64
-  property project : Project
-  property cursus_ids : Array(UInt64)
-  property retirable_at : String?
+    property id : UInt64
+    property occurrence : Int32
+    property final_mark : Int32?
+    property status : String
+    property current_team_id : UInt64
+    property project : Project
+    property cursus_ids : Array(UInt64)
+    property retirable_at : String?
 
-  @[JSON::Field(key: "validated?")]
-  property is_validated : Bool?
+    @[JSON::Field(key: "validated?")]
+    property is_validated : Bool?
+  end
 end
